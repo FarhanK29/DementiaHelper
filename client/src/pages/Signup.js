6df4a7e0ba74eba796061976ef3653a1015073b2
Farhan Khan <farhan.k0429@gmail.com>
2024-10-26 17:41:58 -0400

    Added pages and server folder

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Signup.css';
+import { Link, useNavigate } from 'react-router-dom'
+
+import React from 'react'
+
+export default function Signup()
+{
+    const [username, setUsername] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    return(
+        <div>Signup</div>
+    )
+}
+
