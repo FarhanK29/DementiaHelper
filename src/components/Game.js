6c7d4ef569eb55ca2b322344adeffef2ea52dae3
Afn377 <afnanthegamer2018@gmail.com>
2024-10-26 17:00:14 -0400

    initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+// src/Game.js
+import React from 'react';
+
+function Game() {
+  return (
+    <div>
+      <h1>Welcome to the Game Page</h1>
+      <p>This is where the game will be located!</p>
+    </div>
+  );
+}
+
+export default Game;
