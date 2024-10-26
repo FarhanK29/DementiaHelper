b75f64622459ff6a486ec90eb56530aed3c6f5b6
Afn377 <64026942+Afn377@users.noreply.github.com>
2024-10-26 17:17:21 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Game from './components/Game';
 
 function App() {
   return (
+    <Router>
       <div>
-        Hello World
-
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/game">Game</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
       </div>
+    </Router>
   );
 }
 
