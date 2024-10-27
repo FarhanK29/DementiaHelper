2e5a8d3f0f44db87f07d9772dcb8b8d6b4ecf04c
Afn377 <afnanthegamer2018@gmail.com>
2024-10-27 00:40:39 -0400

    signup

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -11,7 +11,7 @@ const Signup = () => {
   const handleSignup = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:8000/api/signup/', {
+      const response = await fetch('http://127.0.0.1:8000/api/signup/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password, email }), // Send the necessary fields
