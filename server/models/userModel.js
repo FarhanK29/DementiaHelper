c23334fbaf6a65d78a56c83c32b0a3570c964b75
Farhan Khan <farhan.k0429@gmail.com>
2024-10-26 20:04:01 -0400

    Added protected routes with auth

---
+++ server/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+},
+
+    {
+        collection: 'users',
+        timestamps: true,
+    },
+
+);
+
+module.exports = mongoose.model('User', UserSchema);
